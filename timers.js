a3bc9e966a3c4818a9cbdeb8c28dfc6cce76776b
Andrew Young <prince.of.utah@gmail.com>
2016-06-02 09:26:27 -0400

    Initial Commit

--- timers.js
+++ timers.js
@@ -1,4 +1,29 @@
+$(document).on("ready", function(){
 
+    var counter = 0;
+    var counterUpdaterBucket;
+    var counterUpdaterBucketIsEmpty = true;
 
+    $("#start").on("click", function(){
+        if(counterUpdaterBucketIsEmpty == true){
+            counterUpdaterBucket = setInterval(function(){
+                counter += 1;
+                $("h1").text(counter);
+            }, 100);
+            counterUpdaterBucketIsEmpty = false;
+        }
+    });
 
+    $("#pause").on("click", function(){
+        clearInterval(counterUpdaterBucket);
+        counterUpdaterBucketIsEmpty = true;
+    });
 
+    $("#reset").on("click", function(){
+        clearInterval(counterUpdaterBucket);
+        counterUpdaterBucketIsEmpty = true;
+        counter = 0;
+        $("h1").text(counter);
+    });
+
+});
